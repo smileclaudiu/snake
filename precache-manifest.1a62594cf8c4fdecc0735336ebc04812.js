8dc597b902e1f4b550ba42c39b77281cfa54b66d
Claudiu <claudiupenet@gmail.com>
2019-08-20 20:09:04 +0300

    Updates

---
+++ precache-manifest.1a62594cf8c4fdecc0735336ebc04812.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61547ab3a383a9bd78355d532c43bb1f",
+    "url": "/snake/index.html"
+  },
+  {
+    "revision": "85e9cb6f870942183a2a",
+    "url": "/snake/static/css/main.fe25211e.chunk.css"
+  },
+  {
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/snake/static/js/2.09aed800.chunk.js"
+  },
+  {
+    "revision": "85e9cb6f870942183a2a",
+    "url": "/snake/static/js/main.40c83e5b.chunk.js"
+  },
+  {
+    "revision": "28fbb7debeb67f585d77",
+    "url": "/snake/static/js/runtime~main.ccc81976.js"
+  }
+]);
